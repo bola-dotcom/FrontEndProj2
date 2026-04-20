5741ce9520df82a8f8a8241487f959e9e1156e1c
unknown <G00462097@atu.ie>
2026-04-20 21:44:46 +0000

    Started the home page of the app

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,20 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { IonCardContent,IonCardHeader,IonCard,IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { RecipeService } from 'src/app/services/recipe.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule,IonCardContent,IonCardHeader,IonCard,IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
+
+
 export class HomePage implements OnInit {
+  recipes: any[] = [];
+  constructor(private recipeService: RecipeService) {}
+  
 
-  constructor() { }
-
-  ngOnInit() {
+ngOnInit() {
+  this.loadRecipes();
   }
 
+  loadRecipes(){
+    this.recipeService.getRecipes().subscribe({
+next:(data:any) => {
+  this.recipes = data.meals || [];
+},
+error:(err) =>{
+  console.error('API error:', err);
+}
+
+    });
+  }
+  
 }
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -6,10 +6,11 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class RecipeService {
+private apiUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
 
   constructor(private http: HttpClient) {}
 
   getRecipes(): Observable<any> {
-    return this.http.get('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+    return this.http.get(this.apiUrl);
   }
 }
