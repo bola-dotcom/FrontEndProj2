1359575e328f1c0500acf415eec3176aef35fe75
bola-dotcom <G00462097@atu.ie>
2026-04-21 22:57:39 +0000

    i am trying to fix te detail page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -2,22 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { RecipeService } from '../../services/recipe.service';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-
+import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, IonicModule]
- 
+  imports: [IonicModule, CommonModule, RouterModule]
 })
 export class HomePage implements OnInit {
 
   recipes: any[] = [];
 
-  constructor(private recipeService: RecipeService) {}
+  constructor(private recipeService: RecipeService,
+    private router: Router
+  ) {}
 
+  goToRecipe(id: string) {
+  this.router.navigate(['/recipe', id]);
+}
   ngOnInit() {
     this.recipeService.getRecipes().subscribe((data: any) => {
       this.recipes = data.meals || [];
