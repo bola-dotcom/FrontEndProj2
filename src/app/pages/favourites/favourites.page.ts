53b8ba21d0154ef2e867653773cab804d29f9a50
bola-dotcom <G00462097@atu.ie>
2026-04-22 23:22:46 +0000

    I implemented a search function

--- src/app/pages/favourites/favourites.page.ts
+++ src/app/pages/favourites/favourites.page.ts
@@ -1,24 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-import { StorageService } from '../../services/storage';
 import { IonicModule } from '@ionic/angular';
+import { StorageService } from '../../services/storage';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.page.html',
   styleUrls: ['./favourites.page.scss'],
   standalone: true,
-  imports: [IonicModule, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, RouterModule]
 })
-export class FavouritesPage implements OnInit {
+export class FavouritesPage {
 
   favourites: any[] = [];
-  constructor(private storage: StorageService) { }
 
-  async ngOnInit() {
+  constructor(private storage: StorageService) {}
+
+  async ionViewWillEnter() {
     const data = await this.storage.get('favourites');
     this.favourites = data || [];
+  }
+  async removeFavourite(id:string){
+    let favourites = await this.storage.get('favourites');
+
+    favourites = favourites.filter((r: any) => r.idMeal !== id);
+    await this.storage.set('favourites', favourites);
+  this.favourites =favourites;
   }
 
 }
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -4,16 +4,20 @@ import { IonicModule } from '@ionic/angular';
 import { RecipeService } from '../../services/recipe.service';
 import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, RouterModule]
+  imports: [IonicModule, CommonModule, RouterModule, FormsModule]
 })
 export class HomePage implements OnInit {
 
   recipes: any[] = [];
+
+filteredRecipes: any[] = [];
+searchTerm: string = '';
 
   constructor(private recipeService: RecipeService,
     private router: Router
@@ -25,6 +29,16 @@ export class HomePage implements OnInit {
   ngOnInit() {
     this.recipeService.getRecipes().subscribe((data: any) => {
       this.recipes = data.meals || [];
+      this.filteredRecipes = this.recipes;
+      
     });
-  }
+   
+}
+
+filterRecipes(){
+  const term = this.searchTerm.toLowerCase();
+
+  this.filteredRecipes = this.recipes.filter((recipe:any) =>
+  recipe.str.Meal.toLowerCase().include(term));
+}
 }
